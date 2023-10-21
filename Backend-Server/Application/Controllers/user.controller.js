b72a8b83ea1a0ce61a9812a6496e986ea098901f
Cruzer0110 <btech15069.22@bitmesra.ac.in>
2023-10-21 10:44:11 +0530

    Added signin controller and password confirmation for users
    The users controllers now have a signin controller that provides back a token with signin informations. Also added the confirm password field for signup controller. Users can now signup and signin with authentication corresponding to their privileges.

--- Backend-Server/Application/Controllers/user.controller.js
+++ Backend-Server/Application/Controllers/user.controller.js
@@ -1,4 +1,5 @@
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const db = require("../DB_Models");
 const User = db.users;
 
@@ -6,47 +7,133 @@ const User = db.users;
 
 exports.signup = (req, res) => {
     //Validate request
-    if (!(req.body.name && req.body.contact && req.body.password)) {
+    if (!(req.body.name && req.body.contact && req.body.password && req.body.confirmPassword)) {
         return res.status(400).send({
             message: "Required fields cannot be empty!"
-        })
+        });
+    }
+    else if (!(req.body.password===req.body.confirmPassword)) {
+        return res.status(401).send({
+            message: "Passwords do not match!"
+        });
     }
 
     //Hash the password
     const saltRounds = 10;
-    bcrypt.hash(req.body.password,saltRounds,(err,hash) => {
+    const hashedPassword = bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
         if (err) {
-            return res.status(500).send({
-                message: err.message || "Some error occured while hashing the password."
+            res.status(500).send({
+                message: err.message || "Internal server error occured! Please try again later."
             });
+            return;
         }
-
-        //Create a User
-        const user = new User({
-            name: req.body.name,
-            contact: req.body.contact,
-            password: hash,
-            address: {
-                street: req.body.address.street,
-                city: req.body.address.city,
-                state: req.body.address.state,
-                pin: req.body.address.pin
-            },
-            role: req.body.role,
-            agency: req.body.agency
-        });
-
-        //Save User in the database
-        user
-            .save(user)
-            .then(data => {
-                res.send(data);
-            })
-            .catch(err => {
-                res.status(500)
-                    .send({
-                        message: err.message || "Some error occured while creating the User."
-                    });
-            });
+        else {
+            return hash;
+        }
     });
+
+    //Create a User
+    const user = new User({
+        name: req.body.name,
+        contact: req.body.contact,
+        password: hashedPassword,
+        email: req.body.email,
+        address: {
+            street: req.body.address.street,
+            city: req.body.address.city,
+            state: req.body.address.state,
+            pin: req.body.address.pin
+        },
+        role: req.body.role,
+        agency: req.body.agency
+    });
+
+    //Save User in the database
+    user
+        .save(user)
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500)
+                .send({
+                    message: "Internal server error occured! Please try again later."
+                });
+        });
+};
+
+//Sign in an existing user and return a JWT token corresponding to their role
+
+exports.signin = (req, res) => {
+
+    const email = req.body.email;
+    const password = req.body.password;
+
+    //Validate request
+    if (!(email && password)) {
+        return res.status(400).send({
+            message: "Required fields cannot be empty!"
+        });
+    }
+
+    //Find user with corresponding sign in request
+    User
+        .find({ email: email })
+        .then(data => {
+            //Return message if no such user found
+            if (!data) {
+                res.status(404).send({
+                    message: "No such email registered! Please signup first!"
+                });
+                return;
+            }
+            else {
+                // Compare password with the stored password
+                bcrypt.compare(password, data.password, (err, same) => {
+                    if (err) {
+                        return res.status(500).send({
+                            message: err.message || "Internal server error occured! Please try again later."
+                        });
+                    } else if (!same) {
+                        //Return message for invalid credentials
+                        return res.status(401).send({
+                            message: "Credentials do not match! Please try again!"
+                        });
+                    }
+                    else {
+                        // Create and send a JWT token with appropriate role as response to the user
+                        jwt
+                            .sign(
+                                {
+                                    id: data.id,
+                                    role: data.role,
+                                    agency: data.agency
+                                },
+                                process.env.JWT_SECRET,
+                                {
+                                    expiresIn: "8h"
+                                },
+                                (err, token) => {
+                                    if (err) {
+                                        return res.status(500).send({
+                                            message: err.message || "Internal server error occured! Please try again later."
+                                        });
+                                    }
+                                    else {
+                                        return res.status(200).send({
+                                            message: "Logged in successfully!",
+                                            token: token
+                                        });
+                                    }
+                                }
+                            );
+                    }
+                });
+            }
+        })
+        .catch(err => {
+            return res.status(500).send({
+                message: err.message || "Internal server error occured! Please try again later."
+            });
+        });
 };
