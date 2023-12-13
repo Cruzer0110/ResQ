1734043f8620d325c1719d5e427b3ca39a3cab34
Cruzer0110 <btech15069.22@bitmesra.ac.in>
2023-12-13 19:45:10 +0530

    Added an outer try-catch block to prevent server from crashing while decoding authorization tokens.

--- Backend-Server/Application/Middleware/middleware.js
+++ Backend-Server/Application/Middleware/middleware.js
@@ -2,22 +2,29 @@ const admin = require('../Server_Config/firebase.config.js');
 
 class Middleware {
     decodeToken(req, res, next) {
-        const token = req.headers.authorization.split(' ')[1];
-        admin
-            .auth().verifyIdToken(token)
-            .then(decodeValue => {
-                return next();
-            }, invalidToken => {
-                return res.status(401).send({
-                    message: 'Invalid token',
-                    token: token
+        try {
+            const token = req.headers.authorization.split(' ')[1];
+            admin
+                .auth().verifyIdToken(token)
+                .then(validToken => {
+                    return next();
+                }, invalidToken => {
+                    return res.status(401).send({
+                        message: 'Invalid token',
+                        token: token
+                    });
+                })
+                .catch(authError => {
+                    res.status(500).send({
+                        message: 'Server error. Please try again later.'
+                    });
                 });
-            })
-            .catch(error => {
-                res.status(500).send({
-                    message: 'Server error. Please try again later.'
-                });
+        } catch (error) {
+            console.log(error);
+            res.status(500).send({
+                message: 'Server error. Please try again later.'
             });
+        }
     };
 };
 
