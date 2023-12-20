746e23e6756a2b7bbb7e31cd53cb3dadc700cc5f
Archishman Nag <145678315+Cruzer0110@users.noreply.github.com>
2023-12-20 11:50:43 +0530

    Socket.IO instance for Location Logs correctly configured and implemented.
    The socket consists of all the routes and takes care of authentication before letting sockets connect to the server, making it safe for use.

--- Backend-Server/WebSockets/locationLogSocket.js
+++ Backend-Server/WebSockets/locationLogSocket.js
@@ -1,14 +1,69 @@
-let { Server } = require('socket.io');
+const { Server } = require('socket.io');
+const axios = require('axios');
+const middleware = require('../Application/Middleware/middleware.js');
 
-class locationLogSocket {
+class LocationLogSocket {
     constructor(server) {
-        this.io = new Server(server, {
-            path: '/locationLogSocket'
+        this.socketIDMap = new Map();
+        this.axiosInitialize();
+        this.initializeSocket(server);
+    }
+
+    axiosInitialize() {
+        this.api = axios.create({
+            baseURL: `http://localhost:8080/api/locationLog`,
+            timeout: 1000,
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': null
+            }
         });
+    }
+
+    initializeSocket(server) {
+        this.io = new Server(server, {
+            path: '/locationLogSocket/'
+        });
+        this.configureMiddleware();
+        this.handleSocketEvents();
+    }
+
+    configureMiddleware() {
+        this.io.use(this.authenticateSocket.bind(this));
+    }
+
+    authenticateSocket(socket, next) {
+        let tokenVerified = middleware.decodeTokenForSocket(socket.handshake.auth.token);
+
+        if (tokenVerified.isValid === true) {
+            this.configureAPIInterceptor(socket);
+            this.socketIDMap.set(socket.id, tokenVerified.uid);
+            return next();
+        } else {
+            const err = new Error("Not Authorized!");
+            err.data = {
+                reason: "Token not valid or expired!"
+            };
+            return next(err);
+        }
+    }
+
+    configureAPIInterceptor(socket) {
+        this.api.interceptors.request.use(
+            config => {
+                config.headers['Authorization'] = `Bearer ${socket.handshake.auth.token}`;
+                return config;
+            }, error => {
+                return Promise.reject(error);
+            }
+        );
+    }
+
+    handleSocketEvents() {
         this.io.on('connection', socket => {
             socket.on('updateLocation', (locationData, room) => {
-                app.post('/api/locationLog', locationData);
-                if (room == '') {
+                this.api.post('/', locationData);
+                if (room === null) {
                     socket.broadcast.emit('newLocation', locationData);
                 } else {
                     socket.to(room).emit('newLocation', locationData);
@@ -16,23 +71,40 @@ class locationLogSocket {
             });
             socket.on('joinRoom', room => {
                 socket.join(room);
-                let locationData = [];
-                socket.in(room).sockets.forEach(element => {
-
-                })
+                this.io.in(room)
+                    .fetchSockets()
+                    .then(sockets => {
+                        const locationPromises = sockets.map(element => {
+                            return this.api
+                                .get(`/`,
+                                    {
+                                        params: {
+                                            id: this.socketIDMap.get(element.id)
+                                        }
+                                    }
+                                )
+                                .then(response => {
+                                    return response.data[0];
+                                });
+                        });
+                        return Promise.all(locationPromises);
+                    })
+                    .then(data => {
+                        socket.emit('newLocation', data);
+                    })
+                    .catch(err => {
+                        console.log(err);
+                    });
             });
             socket.on('leaveRoom', room => {
                 socket.leave(room);
             });
-            socket.on('disconnect', (lastLocationData) => {
+            socket.on('disconnect', () => {
+                this.socketIDMap.delete(socket.id);
                 socket.leaveAll();
-                app.post('/api/locationLog', lastLocationData);
             });
-            socket.on(error => {
-                console.log(error);
-            })
         });
     }
 }
 
-module.exports = locationLogSocket;
+module.exports = LocationLogSocket;
