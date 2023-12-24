4b211eab3ff16773d93ef064f79c04746cb6465c
Archishman Nag <145678315+Cruzer0110@users.noreply.github.com>
2023-12-24 17:23:27 +0530

    Retrieved the firebase ID from the verified token to be used by the next middlewares.
    This adds the Firebase ID of the user to the request. Documentation for the functions have been added in the file.

--- Backend-Server/Application/Middleware/middleware.js
+++ Backend-Server/Application/Middleware/middleware.js
@@ -1,12 +1,22 @@
 const admin = require('../Server_Config/firebase.config.js');
 
 class Middleware {
+
+    /**
+     * @description Middleware to verify the token in HTTPS requests.
+     * This middleware is invoked before all the HTTPS requests.
+     * 
+     * @param {import('express').Request} req : Request object
+     * @param {import('express').Response} res : Response object
+     * @param {import('express').NextFunction} next : Next middleware function
+     */
     decodeToken(req, res, next) {
         try {
             const token = req.headers.authorization.split(' ')[1];
             admin
                 .auth().verifyIdToken(token)
                 .then(validToken => {
+                    req.body.userID = validToken.uid;
                     return next();
                 }, invalidToken => {
                     return res.status(401).send({
@@ -21,6 +31,12 @@ class Middleware {
             });
         }
     };
+
+    /**
+     * @description Middleware to verify the token in socket connections.
+     * @param {string} token : The JWT in socket connections to be verified
+     * @returns {object} : Returns an object with the validity of the token and the uid of the user
+     */
     decodeTokenForSocket(token) {
         try {
             admin
